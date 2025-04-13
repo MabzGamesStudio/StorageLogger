dae324946be87da9c16049e01eb1fe72c24020cd
Matt Lips <mabzgamesstudio@gmail.com>
2025-04-13 16:15:31 -0400

    Commented StorageLoggerApp file, wrapped test flag for AdMob in debug mode

--- StorageLogger/StorageLoggerApp.swift
+++ StorageLogger/StorageLoggerApp.swift
@@ -8,13 +8,29 @@
 import SwiftUI
 import GoogleMobileAds
 
+/// The main entry point of the StorageLogger SwiftUI application.
+/// Configures Google Mobile Ads and launches the main view of the app.
 @main
 struct StorageLogger: App {
+    
+    /// Initializes the app by configuring Google Mobile Ads (AdMob).
+    ///
+    /// - Sets the test device identifier to ensure ads behave in test mode on the specified device if in debug mode.
+    /// - Starts the Mobile Ads SDK.
     init() {
+        
+        // Show test ads only in development
+        #if DEBUG
         MobileAds.shared.requestConfiguration.testDeviceIdentifiers = [ "225a96dcd01cf6e60e6bebe2b9c21836" ]
+        #endif
+        
+        // Initialize the Google Mobile Ads SDK
         MobileAds.shared.start { _ in }
     }
 
+    /// The main content scene of the app.
+    ///
+    /// - Returns a `WindowGroup` containing the app's root view, `EntriesListView`.
     var body: some Scene {
         WindowGroup {
             EntriesListView()
